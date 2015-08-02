74a70f5a23164c022d8b73dec1078d0b0efcd5df
s1031430 <w5151381guy@gmail.com>
2015-08-02 17:07:18 +0800

    CPE code

---
+++ CPE score/Untitled1.cpp
@@ -0,0 +1,30 @@
+#include<iostream>
+#include<cstring>
+using namespace std;
+int score(string);
+int main()
+{
+	string str;
+	cout<<"";
+	cin>>str;
+	cout<<score(str);
+	return 0;
+}
+int score(string str)
+{
+	int a=0,sum=0;
+	for(int i=0;i<str.length();i++)
+	{
+		if(str[i]=='o')
+		{
+			a++;
+			sum=sum+a;
+		}
+		if(str[i]=='x')
+		{
+			a=0;
+			sum=sum+a;
+		}	
+	}
+	return sum;
+}
